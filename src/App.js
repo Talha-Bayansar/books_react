abe1aef14c2a6e9dfb4928bc5731c36b0383babb
Talha Bayansar <talha.bayansar@hotmail.com>
2021-01-13 19:59:17 +0100

    connected to backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const StyledApp = styled.div`
-  font-size: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
 
 function App() {
+  const [data, setData] = useState();
+
+  async function getData(){
+    const response = await fetch("http://localhost:3001/");
+    const body = await response.json();
+    setData(body);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <StyledApp>
-      Test
+      {
+        data ? data.map(d => 
+        <span>{d.message}</span>
+      ) : <span>There is no data</span>
+      }
+      
     </StyledApp>
   );
 }
